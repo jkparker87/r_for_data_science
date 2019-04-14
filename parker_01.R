843f7767ebfaa7f42038bd3c92a60aaa3316f29f
jkparker87 <jacob.parker@gmail.com>
2019-04-14 16:53:37 -0700

    Created 2 scatter plots and added aesthetic mappings

--- parker_01.R
+++ parker_01.R
@@ -1,1 +1,13 @@
-# This is a test file
+# Load the required packages for the Chapter 3 exercises
+library(tidyverse)
+
+# Load the MPG dataset from the ggplot2 package
+mpg <- mpg
+
+# Create a scatter plot of displacement and highway MPG's
+plot1 <- ggplot(data = mpg)+
+  geom_point(aes(x = displ, y = hwy))
+
+# Create a scatter plot of highway MPG and cylinders
+plot2 <- ggplot(data = mpg)+
+  geom_point(aes(x = cyl, y = hwy))
