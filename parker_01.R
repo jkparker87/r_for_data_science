bb05a87c0978e6a8929769599003708f7966f929
jkparker87 <jacob.parker@gmail.com>
2019-04-14 21:31:34 -0700

    Chapter 5 exercises

--- parker_01.R
+++ parker_01.R
@@ -4,10 +4,27 @@ library(tidyverse)
 # Load the MPG dataset from the ggplot2 package
 mpg <- mpg
 
-# Create a scatter plot of displacement and highway MPG's
-plot1 <- ggplot(data = mpg)+
-  geom_point(aes(x = displ, y = hwy))
+# Load the diamonds dataset
+diamonds <- diamonds
 
-# Create a scatter plot of highway MPG and cylinders
-plot2 <- ggplot(data = mpg)+
-  geom_point(aes(x = cyl, y = hwy))
+# Create a scatter plot of displacement and highway MPG's
+plot <- ggplot(data = mpg) +
+  geom_point(aes(x = displ, y = hwy), position = "jitter")
+
+# Smooth line graph
+ggplot(data = mpg) +
+  geom_smooth(aes(x = displ, y = hwy))
+
+# Bar chart
+ggplot(diamonds) +
+  geom_bar(aes(x = cut, fill = clarity), position = "dodge")
+
+# Data transformation exercises
+# Load the NYC flights dataset
+library(nycflights13)
+flights <- flights
+
+# Filtered flights dataset
+filtered_flights <- flights %>%
+  filter(month == 1, day == 1)
+
